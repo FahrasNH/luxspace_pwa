39f6b4b09c9383c5792eeb64bceaeed59480fda5
FahrasNH <fahras1998@yahoo.com>
2021-12-12 08:17:50 +0700

    Create modal func

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,32 @@
-function Hero()  {
-  return(
-      <section className="flex items-center hero">
-      <div
-        className="w-full absolute z-20 inset-0 md:relative md:w-1/2 text-center flex flex-col justify-center hero-caption"
-      >
+import { useState } from 'react'
+import Modal from './Modal'
+
+function Hero() {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleShowModal = () => {
+    setShowModal(!showModal)
+  }
+
+  return (
+    <section className="flex items-center hero">
+      <div className="w-full absolute z-20 inset-0 md:relative md:w-1/2 text-center flex flex-col justify-center hero-caption">
         <h1 className="text-3xl md:text-5xl leading-tight font-semibold">
-          The Room <br className="" />You've Dreaming
+          The Room <br className="" />
+          You've Dreaming
         </h1>
         <h2 className="px-8 text-base md:px-0 md:text-lg my-6 tracking-wide">
           Kami menyediakan furniture berkelas yang
-          <br className="hidden lg:block" />membuat ruangan terasa homey
+          <br className="hidden lg:block" />
+          membuat ruangan terasa homey
         </h2>
         <div>
           <a
             href="#browse-the-room"
             className="bg-pink-400 text-black hover:bg-black hover:text-pink-400 rounded-full px-8 py-3 mt-4 inline-block flex-none transition duration-200"
-            >Explore Now</a
           >
+            Explore Now
+          </a>
         </div>
       </div>
       <div className="w-full inset-0 md:relative md:w-1/2">
@@ -25,18 +35,7 @@ function Hero()  {
           <div className="overlay right-0 bottom-0 md:inset-0">
             <button
               className="video hero-cta focus:outline-none z-30 modal-trigger"
-              data-content='<div className="w-screen pb-56 md:w-88 md:pb-56 relative z-50">
-              <div className="absolute w-full h-full">
-                <iframe
-                  width="100%"
-                  height="100%"
-                  src="https://www.youtube.com/embed/3h0_v1cdUIA"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                ></iframe>
-              </div>
-            </div>'
+              onClick={handleShowModal}
             ></button>
           </div>
           <img
@@ -46,6 +45,8 @@ function Hero()  {
           />
         </div>
       </div>
+
+      { showModal && <Modal handleShowModal={handleShowModal} /> }
     </section>
   )
 }
